1aee63aa744f8b6bc30aa6e9e40623826a401ffb
NhutHao5504 <haonhut.01012004@gmail.com>
2025-10-02 15:50:26 +0700

    .env vd44

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express()
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello world updated");
+    res.send("Hello world nodemon");
 })
 
 app.listen(8080, () => {
     console.log(`My app is running on port: ${PORT}`);
+    console.log("env port: ", process.env.PORT);
     
 })
